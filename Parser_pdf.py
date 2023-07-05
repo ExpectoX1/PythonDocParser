4c2505e81b70166d369a5e547175c1c8872e3ce7
Siddharth Kumar <79239242+ExpectoX1@users.noreply.github.com>
2023-07-05 14:52:43 +0530

    Update Parser_pdf.py

--- Parser_pdf.py
+++ Parser_pdf.py
@@ -1,82 +1,83 @@
 import os
-import PyPDF2
+import pdfplumber
 import re
 
 def parse_pdf(file_path):
-    pdf_file = open(file_path, 'rb')
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
-    num_pages = len(pdf_reader.pages)
+    with pdfplumber.open(file_path) as pdf:
+        num_pages = len(pdf.pages)
 
-    # Regular expression patterns to match headings
-    heading_pattern = re.compile(r'^\s*\d+(?:\.\d+)*\s*(.*)$')
-    sub_heading_pattern = re.compile(r'^\s*\d+(?:\.\d+)*\s*(.*)$')
-    sub_sub_heading_pattern = re.compile(r'^\s*\d+(?:\.\d+)*\s*(.*)$')
-    
+        # Regular expression patterns to match headings
+        heading_pattern = re.compile(r'^\s*\d+(?:\.\d+)*\s*(.*)$')
+        sub_heading_pattern = re.compile(r'^\s*\d+(?:\.\d+)*\s*(.*)$')
+        sub_sub_heading_pattern = re.compile(r'^\s*\d+(?:\.\d+)*\s*(.*)$')
 
-    # Create a folder for the extracted text files
-    folder_name = "Extracted_Text"
-    os.makedirs(folder_name, exist_ok=True)
+        # Create a folder for the extracted text files
+        folder_name = "Extracted_Text"
+        os.makedirs(folder_name, exist_ok=True)
 
-    for page_num in range(num_pages):
-        page = pdf_reader.pages[page_num]
-        content = page.extract_text()
+        for page_num in range(num_pages):
+            page = pdf.pages[page_num]
+            content = page.extract_text()
 
-        # Split the content into lines
-        lines = content.split('\n')
+            # Split the content into lines
+            lines = content.split('\n')
 
-        heading = ''
-        sub_heading = ''
-        sub_sub_heading = ''
-        text = ''
-        for line in lines:
-            line = line.strip()
+            heading = ''
+            sub_heading = ''
+            sub_sub_heading = ''
+            text = ''
+            tables = page.extract_tables()  # Extract all tables from the page
 
-            # Check if the line matches the heading pattern
-            match_heading = heading_pattern.match(line)
-            match_sub_heading = sub_heading_pattern.match(line)
-            match_sub_sub_heading = sub_sub_heading_pattern.match(line)
+            for line in lines:
+                line = line.strip()
 
-            if match_heading:
-                # Save the previous heading, subheading, and text to a file
-                if heading and text:
-                    save_to_file(folder_name, heading, sub_heading, sub_sub_heading, text)
+                # Check if the line matches the heading pattern
+                match_heading = heading_pattern.match(line)
+                match_sub_heading = sub_heading_pattern.match(line)
+                match_sub_sub_heading = sub_sub_heading_pattern.match(line)
 
-                # Update the heading, subheading, and sub-subheading
-                heading = match_heading.group(1)
-                sub_heading = ''
-                sub_sub_heading = ''
-                text = ''
+                if match_heading:
+                    # Save the previous heading, subheading, text, and tables to a file
+                    if heading and (text or tables):
+                        save_to_file(folder_name, heading, sub_heading, sub_sub_heading, text, tables)
 
-            elif match_sub_heading:
-                # Save the previous subheading and text to a file
-                if sub_heading and text:
-                    save_to_file(folder_name, heading, sub_heading, sub_sub_heading, text)
+                    # Update the heading, subheading, and sub-subheading
+                    heading = match_heading.group(1)
+                    sub_heading = ''
+                    sub_sub_heading = ''
+                    text = ''
+                    tables = []
 
-                # Update the subheading and sub-subheading
-                sub_heading = match_sub_heading.group(1)
-                sub_sub_heading = ''
-                text = ''
+                elif match_sub_heading:
+                    # Save the previous subheading, text, and tables to a file
+                    if sub_heading and (text or tables):
+                        save_to_file(folder_name, heading, sub_heading, sub_sub_heading, text, tables)
 
-            elif match_sub_sub_heading:
-                # Save the previous sub-subheading and text to a file
-                if sub_sub_heading and text:
-                    save_to_file(folder_name, heading, sub_heading, sub_sub_heading, text)
+                    # Update the subheading and sub-subheading
+                    sub_heading = match_sub_heading.group(1)
+                    sub_sub_heading = ''
+                    text = ''
+                    tables = []
 
-                # Update the sub-subheading
-                sub_sub_heading = match_sub_sub_heading.group(1)
-                text = ''
+                elif match_sub_sub_heading:
+                    # Save the previous sub-subheading, text, and tables to a file
+                    if sub_sub_heading and (text or tables):
+                        save_to_file(folder_name, heading, sub_heading, sub_sub_heading, text, tables)
 
-            else:
-                # Add the line to the text
-                text += line + '\n'
+                    # Update the sub-subheading
+                    sub_sub_heading = match_sub_sub_heading.group(1)
+                    text = ''
+                    tables = []
 
-        # Save the last heading, subheading, sub-subheading, and text to a file
-        if heading and text:
-            save_to_file(folder_name, heading, sub_heading, sub_sub_heading, text)
+                else:
+                    # Add the line to the text
+                    text += line + '\n'
 
-    pdf_file.close()
+            # Save the last heading, subheading, sub-subheading, text, and tables to a file
+            if heading and (text or tables):
+                save_to_file(folder_name, heading, sub_heading, sub_sub_heading, text, tables)
 
-def save_to_file(folder, heading, sub_heading, sub_sub_heading, text):
+def save_to_file(folder, heading, sub_heading, sub_sub_heading, text, tables):
     # Replace any invalid characters in the headings with underscores
     heading = re.sub(r'[^\w\s-]', '_', heading)
     sub_heading = re.sub(r'[^\w\s-]', '_', sub_heading)
@@ -86,10 +87,15 @@ def save_to_file(folder, heading, sub_heading, sub_sub_heading, text):
     # Create a file path with the folder and headings
     filename = heading + sub_heading + sub_sub_heading
 
-    if (filename_pattern.match(filename) == None):
+    if filename_pattern.match(filename) is None:
         file_path = os.path.join(folder, f"{filename}.txt")
         with open(file_path, 'w') as file:
-            file.write(text)
+            if text.strip():
+                file.write(text.strip())
+            if tables:
+                for table in tables:
+                    for row in table:
+                        file.write('\t'.join(row) + '\n')
     else:
         # Handle the case when the filename does not match the specified pattern
         # You can choose to skip saving the file or modify the filename to match the desired format
